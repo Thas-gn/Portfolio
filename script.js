0ff3eea06c1163d9a40fe1cb751ad2f44122d8e2
Thas-gn <sowthas69@gmail.com>
2025-09-21 17:49:21 +0000

    Premier commit -Portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+//Smooth scroll pour les liens internes
+
+document.querySelectorAll('a[href^="#"]').forEach(a =>{
+    a.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(a.getAttribute('href')).scrollIntoView(
+            {behavior: 'smooth'}
+        )
+    });
+});
